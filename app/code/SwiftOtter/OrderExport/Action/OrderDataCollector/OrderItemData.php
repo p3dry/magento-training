cd812dc68ac8c1dc11136cfefebe1fb6a5c936fb
Pedro <pedro.hernandez@infracommerce.lat>
2023-08-29 01:12:20 -0500

    Chapter 2.3.5 - GetExportItems

--- app/code/SwiftOtter/OrderExport/Action/OrderDataCollector/OrderItemData.php
+++ app/code/SwiftOtter/OrderExport/Action/OrderDataCollector/OrderItemData.php
@@ -3,30 +3,53 @@
 namespace SwiftOtter\OrderExport\Action\OrderDataCollector;
 
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderItemInterface;
+use SwiftOtter\OrderExport\Action\GetOrderExportItems;
 use SwiftOtter\OrderExport\Api\OrderDataCollectorInterface;
 use SwiftOtter\OrderExport\Model\HeaderData;
 
 class OrderItemData implements OrderDataCollectorInterface
 {
+    /** @var GetOrderExportItems  */
+    private GetOrderExportItems $getOrderExportItems;
+
+    public function __construct(
+        GetOrderExportItems $getOrderExportItems
+    ) {
+        $this->getOrderExportItems = $getOrderExportItems;
+    }
+
     public function collect(OrderInterface $order, HeaderData $headerData): array
     {
+        $items = [];
+        $orderItems = $this->getOrderExportItems->execute($order);
+        foreach ($orderItems as $orderItem) {
+            $items[] = $this->transform($orderItem);
+        }
         return [
-            'items' => [
-                [
-                    'sku' => 'AB-1',
-                    'qty' => 1,
-                    'item_price' => 5,
-                    'item_cost' => 3,
-                    'total' => 5
-                ],
-                [
-                    'sku' => 'AB-2',
-                    'qty' => 2,
-                    'item_price' => 10,
-                    'item_cost' => 5,
-                    'total' => 20
-                ]
-            ]
+            'items' => $items
         ];
     }
+
+    /**
+     * @return array{
+     *     sku: string,
+     *     qty: float|null,
+     *     item_price: float|null,
+     *     item_cost: float|null,
+     *     total: float|null
+     * }
+     */
+    private function transform(OrderItemInterface $orderItem): array
+    {
+        return [
+            'sku' => $orderItem->getSku(),
+            'qty' => $orderItem->getQtyOrdered(),
+            'item_price' => $orderItem->getBasePrice(),
+            'item_cost' => $orderItem->getBaseCost(),
+            'total' => $orderItem->getBaseRowTotal(),
+            'type' => $orderItem->getProductType(),
+        ];
+    }
+
 }
